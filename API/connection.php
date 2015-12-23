b27459f710932654fe0ea81181bb14c6f1392a15
ppyoosuf <pp.yoosuf@gmail.com>
2015-12-23 21:52:36 +0530

    Create connection.php

---
+++ API/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+define('SERVER', "localhost");
+define('DB', "recruitment");
+define('USERNAME', "root");
+define('PASSWORD', "");
+
+
+?>
